9958b3306e32d0fc4ca9bb1728736486c153229c
Aryan Gupta <118144312+agupt295@users.noreply.github.com>
2026-08-07 17:52:56 -0700

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.java
@@ -0,0 +1,16 @@
+class KthLargest {
+    Queue<Integer> queue;
+    int k;
+    public KthLargest(int k, int[] nums) {
+        queue = new PriorityQueue<>();
+        this.k = k;
+        for(int num: nums) { queue.add(num); }
+        while(queue.size() > k) { queue.remove(); }
+    }
+    
+    public int add(int val) {
+        queue.add(val);
+        if(queue.size() > k) { queue.remove(); }
+        return queue.peek();
+    }
+}
