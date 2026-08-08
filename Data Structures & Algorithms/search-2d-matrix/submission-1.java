b2be9946270270539badecd0546d3e5e293c2965
Aryan Gupta <118144312+agupt295@users.noreply.github.com>
2026-08-08 10:58:55 -0700

    Add: search-2d-matrix - submission-1

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.java
@@ -0,0 +1,66 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // return binary(binary2D(matrix, target), target);
+        return binaryOnePass(matrix, target);
+    }
+
+    public boolean binaryOnePass(int[][] matrix, int target) {
+        int rows = matrix.length, cols = matrix[0].length;
+        int left = 0, right = rows*cols - 1;
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            int midNumber = matrix[mid/cols][mid%cols];
+            if(target == midNumber) return true;
+            if(target < midNumber) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public boolean binary(int[] arr, int target) {
+        int left = 0, right = arr.length-1;
+        while(left <= right) {
+            int mid = left + (right-left)/2;
+            if(arr[mid] == target) {
+                return true;
+            }
+            if(arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    public int[] binary2D(int[][] matrix, int target){
+        int left = 0, right = matrix.length-1;
+        while(left < right){
+            int mid = left + (right-left)/2;
+            if(
+                target >= getFirst(matrix[mid]) &&
+                target <= getLast(matrix[mid])
+            ) {
+                return matrix[mid];
+            }
+            
+            if(target < getFirst(matrix[mid])) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return  matrix[left];
+    }
+
+    public int getFirst(int[] arr) {
+        return arr[0];
+    }
+    
+    public int getLast(int[] arr) {
+        return arr[arr.length-1];
+    }
+}
